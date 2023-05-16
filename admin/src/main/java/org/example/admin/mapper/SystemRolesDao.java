9a7f92c89038f3867269f4a9febd045621f863e5
sunli <sunli@qq.com>
2023-05-16 12:27:39 +0800

    管理员角色管理增删改查

---
+++ admin/src/main/java/org/example/admin/mapper/SystemRolesDao.java
@@ -0,0 +1,7 @@
+package org.example.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.common.entity.SystemAdminRoles;
+
+public interface SystemRolesDao extends BaseMapper<SystemAdminRoles> {
+}
