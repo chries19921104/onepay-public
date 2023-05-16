b60e3a60c415aafad21e236d5be9a33869c51695
z-mingyang <2781098547@qq.com>
2023-05-16 12:38:58 +0800

    添加管理员菜单模块

---
+++ admin/src/main/java/org/example/admin/mapper/SystemMenuMapper.java
@@ -0,0 +1,17 @@
+package org.example.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.common.entity.SystemMenu;
+
+/**
+* <p>
+* system_menu Mapper 接口
+* </p>
+*
+* @author Administrator
+* @since 2023-05-11 16:35:15
+*/
+public interface SystemMenuMapper extends BaseMapper<SystemMenu> {
+
+
+}
---
+++ admin/src/main/java/org/example/admin/service/SystemMenuService.java
@@ -0,0 +1,66 @@
+package org.example.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.example.common.base.CommResp;
+import org.example.common.entity.SystemMenu;
+
+
+/**
+* <p>
+* system_menu Service 接口
+* </p>
+*
+* @author Administrator
+* @since 2023-05-11 16:34:34
+*/
+public interface SystemMenuService extends IService<SystemMenu> {
+
+    /**
+     * 菜单分页列表
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    public CommResp listPage(Integer pageNum, Integer pageSize);
+
+    /**
+     * 菜单删除（逻辑删除）
+     * @param id
+     * @return
+     */
+    public CommResp deleteById(Integer id);
+
+    /**
+     * 菜单新增
+     * @param systemMenu
+     * @return
+     */
+    public CommResp create(SystemMenu systemMenu);
+
+    /**
+     * 菜单修改
+     * @param systemMenu
+     * @return
+     */
+    public CommResp update(SystemMenu systemMenu);
+
+    /**
+     *查询单个菜单（编辑菜单回显）
+     * @param id
+     * @return
+     */
+    public CommResp getId(Integer id);
+
+    /**
+     *获取所有的父级名
+     * @return
+     */
+    public CommResp getPidName();
+
+    /**
+     * 获取父级下所有的子菜单
+     * @param id
+     * @return
+     */
+    public CommResp getIdName(Integer id);
+}
