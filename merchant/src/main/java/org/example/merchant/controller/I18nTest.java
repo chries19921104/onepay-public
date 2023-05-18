05bfaede8fb11416412e766831ef687362d2ccfc
du <2670855390@qq.com>
2023-05-18 11:01:34 +0800

    国际化模板（待完善）

--- merchant/src/main/java/org/example/merchant/controller/I18nTest.java
+++ merchant/src/main/java/org/example/merchant/controller/I18nTest.java
@@ -6,18 +6,26 @@ import org.example.common.utils.MessageUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @Api(tags = "国际化测试")
 @RestController
-@RequestMapping(value = "/test/i18n/")
+@RequestMapping(value = "/test/i18n")
 public class I18nTest {
 
+    // 固定的登录界面
+    private static final List<String> list= Arrays
+            .asList("MasterBackOffice","Password","login","Username");
 
     @ApiOperation(value = "国际化测试")
     @GetMapping("/test")
-    public String testInfo(HttpServletRequest request,String str) {
-        return MessageUtil.getLocale(str);
+    public Map<String,String> testInfo() {
+        Map<String,String> map=new HashMap<>();
+        // 查询出需要国际化的值，返回固定格式给前端，后续用json
+        for (String s : list) {
+            map.put(s,MessageUtil.getLocale(s));
+        }
+
+        return map;
     }
 }
