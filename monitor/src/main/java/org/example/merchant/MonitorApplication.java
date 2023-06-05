cb36191638131a6088764ef7eb7f8f3765c9f7fe
ocean <523040336@qq.com>
2023-06-05 15:20:41 +0800

    增加 监控模块

---
+++ monitor/src/main/java/org/example/merchant/MonitorApplication.java
@@ -0,0 +1,20 @@
+package org.example.merchant;
+
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
+
